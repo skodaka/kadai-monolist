4eb55cc038b77b95912be9bfdbd1ebaabb820611
s.kodaka <minority585858@gmail.com>
2018-04-24 17:07:58 +0000

    Have ranking

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -12,9 +12,22 @@ class RankingController extends Controller
     public function want()
     {
         $items = \DB::table('item_user')->join('items', 'item_user.item_id', '=', 'items.id')->select('items.*', \DB::raw('COUNT(*) as count'))->where('type', 'want')->groupBy('items.id')->orderBy('count', 'DESC')->take(10)->get();
+        $title = 'Wants';
 
         return view('ranking.want', [
             'items' => $items,
+            'title' => $title,
+        ]);
+    }
+    
+    public function have()
+    {
+        $items = \DB::table('item_user')->join('items', 'item_user.item_id', '=', 'items.id')->select('items.*', \DB::raw('COUNT(*) as count'))->where('type', 'have')->groupBy('items.id')->orderBy('count', 'DESC')->take(10)->get();
+        $title = 'Haves';
+        
+        return view('ranking.have', [
+            'items' => $items,
+            'title' => $title,
         ]);
     }
 }
